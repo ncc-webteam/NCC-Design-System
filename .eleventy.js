248f90c2d8c504319f573f54e4cc44d0ad89e0e3
Kai-NCC <kajetan.orski@norfolk.gov.uk>
2022-01-21 14:28:13 +0000

    Just a small test

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+let Nunjucks = require("nunjucks");
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy("./src/css/");
+
+  let nunjucksEnvironment = new Nunjucks.Environment(
+    new Nunjucks.FileSystemLoader("src/_includes")
+  );
+
+  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: "src",
+      output: "public"
+    },
+  };
+};
